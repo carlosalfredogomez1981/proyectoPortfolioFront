c9a44dfa70c0e832061b917d8e9e6a17d727fc45
CARLOS <carlosalfredogomez1981@gmail.com>
2023-04-28 15:19:03 -0300

    TS de circulos progreso listo

--- src/app/componentes/contenido/circulos-progreso/circulos-progreso.component.ts
+++ src/app/componentes/contenido/circulos-progreso/circulos-progreso.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Skill } from 'src/app/model/skill';
+import { SkillService } from 'src/app/servicios/skill.service';
+import { TokenService } from 'src/app/servicios/token.service';
 
 
 
@@ -7,14 +10,41 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './circulos-progreso.component.html',
   styleUrls: ['./circulos-progreso.component.css']
 })
-export class CirculosProgresoComponent   {
-  
-  isDivHidden = true;
+export class CirculosProgresoComponent implements OnInit  {
+  skill: Skill [] = [];
+ 
+  constructor(private skillS: SkillService, private tokenS: TokenService){}
 
-  toggleDiv() {
-    this.isDivHidden = !this.isDivHidden;
+  isLogged = false;
+
+ngOnInit(): void {
+   this.cargarSkills();
+   if(this.tokenS.getToken()){
+    this.isLogged = true;
+  } else {
+    this.isLogged = false;
+  } 
+}
+
+cargarSkills():void{
+  this.skillS.lista().subscribe(
+    data => {this.skill = data}
+  )
+}
+
+
+
+delete(id: number){
+  if(id != undefined){
+    this.skillS.delete(id).subscribe(
+      data => {this.cargarSkills();},
+      err => {alert("error al borrar")}
+    )
   }
 
-  
+
+
+
+
   
 }
